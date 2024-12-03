8318f73d468bac66c6e82a9046d7325615f04b50
PAQUERIAUD Baptiste <baptiste.paqueriaud@etu.univ-amu.fr>
2024-12-04 00:35:31 +0100

    Initialize CMake and Makefile build systems; add basic project structure and hello world functionality

---
+++ templates/project/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/include.hpp"
+
+// Main function
+int main(int argc, char** argv) {
+
+    // Function from include.hpp
+    print_hello_world();
+
+    return EXIT_SUCCESS;
+}
